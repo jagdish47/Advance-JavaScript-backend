a6b222d53f70ca7a96b28968b809f967cd0cedeb
jagdish47 <jagdishkumawat81@gmail.com>
2023-08-23 09:51:48 +0530

    String Coersion

--- Coersion toNumber toString and toBoolean/toString.js
+++ Coersion toNumber toString and toBoolean/toString.js
@@ -9,3 +9,18 @@
 
 //Object -> Prmitive Value ? ToPrimitive(argument)
 //       -> Return ? ToString(PrimValue)
+
+let obj = {};
+console.log(obj.toString()); //[object, object]
+
+let arr = [];
+console.log(arr.toString()); //empty
+
+let arr1 = [1, 2, 3];
+console.log(arr1.toString()); //1,2,3
+
+console.log(true.toString()); //true
+console.log(false.toString()); //false
+
+let a = undefined;
+console.log(a.toString());
