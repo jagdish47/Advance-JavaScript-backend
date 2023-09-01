84a04ecbb279a32712e92c61eb60ede63b54ee22
jagdish47 <jagdishkumawat81@gmail.com>
2023-09-01 12:52:44 +0530

    Chainig in promise

--- Advanced Async with Promises/Promises-3/promiseDownloadDummyData.js
+++ Advanced Async with Promises/Promises-3/promiseDownloadDummyData.js
@@ -9,6 +9,35 @@ function download(url) {
 }
 
 //this time your not giving access to third party
-download("www.xyz.com").then(function fullFillHandler(value) {
-  console.log("Content Downloaded is", value);
+// download("www.xyz.com").then(function fullFillHandler(value) {
+//   console.log("Content Downloaded is", value);
+// });
+
+// download("www.xyz.com").then(function fullFillHandler(value) {
+//   console.log("Content Downloaded is", value);
+// });
+
+// download("www.xyz.com").then(function fullFillHandler(value) {
+//   console.log("Content Downloaded is", value);
+// });
+//You can handle it Multiple time as well
+
+const x = download("www.xyz.com");
+
+x.then(
+  function fullfillHandler(value) {
+    console.log("Content Downloaded is1", value);
+  },
+  function rejectHandler(value) {
+    console.log("rejected with", value);
+  }
+).then(function newFullFillHandler(value) {
+  console.log("value from chained then promise", value);
 });
+
+//You use then again here also it's called Chained promise
+
+/**
+ * (.then) will return a new Promise
+ *
+ */
